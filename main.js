25b3ccaee584a4cd0513b1097735cc2a873d63cd
Bruno Santos <dev02@whatsmenu.com.br>
2025-03-11 22:35:15 -0300

    feat: implementa atualização automática e ajusta configurações
    - Adiciona .env ao .gitignore
    - Remove título do index.html
    - Configura auto-updater no main.js
    - Atualiza versão do app para 1.0.2
    - Adiciona electron-log como dependência
    - Configura publicação no GitHub no package.json

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 import { app, BrowserWindow } from 'electron'
+import log from "electron-log";
+import pkg from "electron-updater";
+const { autoUpdater } = pkg;
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    title: `App ${app.getVersion()}`,
   })
 
   win.loadFile('index.html')
@@ -11,4 +15,21 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
-})
+  setInterval(() => {
+    autoUpdater.checkForUpdatesAndNotify();
+  }, 1000 * 10)
+});
+
+// Configurando logs
+autoUpdater.logger = log;
+log.transports.file.level = "info";
+
+// Eventos do autoUpdater
+autoUpdater.on("update-available", () => {
+  log.info("Nova atualização disponível.");
+});
+
+autoUpdater.on("update-downloaded", () => {
+  log.info("Atualização baixada. Aplicando...");
+  autoUpdater.quitAndInstall();
+});
